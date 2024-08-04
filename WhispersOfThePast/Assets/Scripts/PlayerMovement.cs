4474b4b151baf5fcebab8698e94099c0fc9ae4cd
Gloryhunter305 <mikylejosh@gmail.com>
2024-08-03 21:25:36 -0400

    Coded game mechanics
    - Social energy (drain rate)
       - UI added

--- WhispersOfThePast/Assets/Scripts/PlayerMovement.cs
+++ WhispersOfThePast/Assets/Scripts/PlayerMovement.cs
@@ -4,22 +4,49 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Player Components")]
     public float speed = 5.0f;
     private Rigidbody2D rb;
+
+    [Header("Game Mechanics")]
+    public int socialHealth = 50;
+    public float currentHealth;
+    public float drainRate = 1f;
+
+    public SocialBarScript socialBar;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        currentHealth = socialHealth;
+        socialBar.SetMaxHealth(socialHealth);
     }
 
     void Update()
     {
+        
+        if (currentHealth >= 0)
+        {
+            DrainHealth(drainRate * Time.deltaTime);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         Vector2 movement = new Vector2(horizontalInput, verticalInput);
 
         rb.velocity = movement * speed;
+    }
+
+
+    void DrainHealth(float damage)
+    {
+        currentHealth -= damage;
+        socialBar.SetHealth(currentHealth);
     }
 
     
